eb3392f52601f0012b13f32fef61c4411c51098e
HualiQiu <huali.qiu@outlook.com>
2020-01-19 22:36:13 +0800

    v-for 遍历

---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: "#app",
+    data() {
+        return {
+            users: ['hua', 'liao', 'zou', 'zeng'],
+            objuser : [
+                {username:'hua',age:18,sex:'men'},
+                {username:'wei',age:29,sex:'men'},
+                {username:'qian',age:20,sex:'women'}
+            ]
+        }
+    }
+})
